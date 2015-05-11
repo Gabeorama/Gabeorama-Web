a52eec5a827b8f3d99c5d8530eef740f52e9b26b
Balonbal <olavbb@hotmail.com>
2015-05-11 22:11:28 +0200

    Code cleanup
    Moved php functions to a separate file for better overview.

---
+++ accounts.php
@@ -0,0 +1,132 @@
+<?php
+$configuration = parse_ini_file("configuration.ini");
+$host = $configuration["host"];
+$user = $configuration["username"];
+$pass = $configuration["password"];
+
+$usernameRegex = "/^[a-zA-Z0-9]{1,16}$/";
+
+$table_name = "users";
+
+function sqlConnect($database) {
+    global $host, $user, $pass;
+    
+    //Open a new connection to specified database
+    $mysqli = new mysqli($host, $user, $pass, $database);
+    
+    //Check for errors
+    if ($mysqli->connect_errno) {
+        echo "ERROR: " . $mysqli->connect_error . "\n";
+        return false;
+    }
+    
+    return $mysqli;
+}
+
+function validateInput($input, $type) {
+    global $usernameRegex;
+    switch ($type) {
+        case "USERNAME":
+            // Usernames should contain only alpanumeric characters and be at most 16 characters long
+            return preg_match("$usernameRegex", $input);
+            break;
+        case "PASSWORD":
+            // I don't feel like dictating password strengths
+            return strlen($input)>3;
+        case "EMAIL":
+            //Check that it is a valid email address
+            return filter_var($input, FILTER_VALIDATE_EMAIL);
+    } 
+}
+
+function registerUser($username, $email, $confirmEmail, $password, $confirmPassword) {
+    global $configuration, $table_name;
+    $error = "";
+
+    /*Validate input*/
+    if (!validateInput($username, "USERNAME")) {
+        $error .= "'" . htmlspecialchars($username) . "' is not a valid username, please make it maximum 16 characters long and use only alphanumeric characters (A-Z and 0-9).\\n";
+    } 
+    if (!validateInput($password, "PASSWORD")) {
+        $error .= "Bad password, use at least 4 characters.\\n";
+    }
+    if (!validateInput($email, "EMAIL")) {
+        $error .= "Bad email, please double check.\\n";
+    }
+    if ($email != $confirmEmail) {
+        $error .= "Email fields should match.\\n";
+    }
+    if ($password != $confirmPassword) {
+        $error .= "Password fields should match.\\n";
+    }
+    
+    //Open database connection
+    $mysqli = sqlConnect($configuration["user_db"]) or die("sql error");
+    
+    //Search for identical emails and usernames
+    $emails = $mysqli->query("SELECT email FROM `$table_name` WHERE `email` = '" . $mysqli->real_escape_string($email) . "'") or die ("sql error");
+    $usernames = $mysqli->query("SELECT username FROM `$table_name` WHERE `username` = '" . $mysqli->real_escape_string($username) . "'") or die("sql error");
+    
+    if ($emails->num_rows > 0) {
+        $error .= "Email already registered, plese request a new password if you have forgotten it.\\n";
+    }
+    
+    if ($usernames->num_rows > 0) {
+        $error .= "Username is already taken, please choose another";
+    }
+    
+    //Close results
+    $usernames->close();
+    $emails->close();
+    
+    //Something went wrong
+    if ($error != "") {
+        ?>
+        <script type="text/javascript">
+            //Alert the user of their error and go back to the previous page
+            alert("<?php echo $error; ?>");
+            history.back();
+        </script>
+        <?php
+        //Legacy code for browsers without javascript
+        die(preg_replace("/\\\\n/", "<br />", $error));
+    }
+    
+    /* Validation complete */
+    
+    //Escape Strings
+    $username = $mysqli->real_escape_string($username);
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    $email = $mysqli->real_escape_string($email);
+    
+    //Register user in the database
+    $registration = $mysqli->query("INSERT INTO `$table_name`(username, email, passwordHash)"
+                                  . "VALUES('$username', '$email', '$password')") or die($mysqli->errorno());
+    $mysqli->close();
+    
+    //Registration complete
+    print("Registration successfull, thank you.\n");
+    return true;
+}
+
+function login($username, $password) {
+    global $configuration, $table_name;
+    
+    $mysqli = sqlConnect($configuration["user_db"]) or die("SQL error");
+    //No SQL injections please
+    $username = $mysqli->real_escape_string($username);
+    
+    //Check logins with both email and username
+    $login = $mysqli->query("SELECT * FROM `$table_name` WHERE username='$username' or email='$username'") or die("SQL error");
+    
+    if (password_verify($password, $login->fetch_array()["passwordHash"])) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function getUsernameRegex() {
+    global $usernameRegex;
+    return $usernameRegex;
+} ?>
--- register.php
+++ register.php
@@ -1,117 +1,11 @@
 <?php
-$configuration = parse_ini_file("configuration.ini");
-$host = $configuration["host"];
-$user = $configuration["username"];
-$pass = $configuration["password"];
+include "accounts.php";
 
-$usernameRegex = "/^[a-zA-Z0-9]{1,16}$/";
-
-function sqlConnect($database) {
-    global $host, $user, $pass;
-    
-    //Open a new connection to specified database
-    $mysqli = new mysqli($host, $user, $pass, $database);
-    
-    //Check for errors
-    if ($mysqli->connect_errno) {
-        echo "ERROR: " . $mysqli->connect_error . "\n";
-        return false;
-    }
-    
-    return $mysqli;
-}
-
-function validateInput($input, $type) {
-    global $usernameRegex;
-    switch ($type) {
-        case "USERNAME":
-            // Usernames should contain only alpanumeric characters and be at most 16 characters long
-            return preg_match("$usernameRegex", $input);
-            break;
-        case "PASSWORD":
-            // I don't feel like dictating password strengths
-            return strlen($input)>3;
-        case "EMAIL":
-            //Check that it is a valid email address
-            return filter_var($input, FILTER_VALIDATE_EMAIL);
-    } 
-}
-
-function registerUser($username, $email, $confirmEmail, $password, $confirmPassword) {
-    global $configuration;
-    $tableName = "users";
-    $error = "";
-    
-    /*Validate input*/
-    if (!validateInput($username, "USERNAME")) {
-        $error .= "'" . htmlspecialchars($username) . "' is not a valid username, please make it maximum 16 characters long and use only alphanumeric characters (A-Z and 0-9).\\n";
-    } 
-    if (!validateInput($password, "PASSWORD")) {
-        $error .= "Bad password, use at least 4 characters.\\n";
-    }
-    if (!validateInput($email, "EMAIL")) {
-        $error .= "Bad email, please double check.\\n";
-    }
-    if ($email != $confirmEmail) {
-        $error .= "Email fields should match.\\n";
-    }
-    if ($password != $confirmPassword) {
-        $error .= "Password fields should match.\\n";
-    }
-    
-    //Open database connection
-    $mysqli = sqlConnect($configuration["user_db"]) or die("sql error");
-    
-    //Search for identical emails and usernames
-    $emails = $mysqli->query("SELECT email FROM `$tableName` WHERE `email` = '" . $mysqli->real_escape_string($email) . "'") or die ("sql error");
-    $usernames = $mysqli->query("SELECT username FROM `$tableName` WHERE `username` = '" . $mysqli->real_escape_string($username) . "'") or die("sql error");
-    
-    if ($emails->num_rows > 0) {
-        $error .= "Email already registered, plese request a new password if you have forgotten it.\\n";
-    }
-    
-    if ($usernames->num_rows > 0) {
-        $error .= "Username is already taken, please choose another";
-    }
-    
-    //Close results
-    $usernames->close();
-    $emails->close();
-    
-    //Something went wrong
-    if ($error != "") {
-        ?>
-        <script type="text/javascript">
-            //Alert the user of their error and go back to the previous page
-            alert("<?php echo $error; ?>");
-            history.back();
-        </script>
-        <?php
-        //Legacy code for browsers without javascript
-        die(preg_replace("/\\\\n/", "<br />", $error));
-    }
-    
-    /* Validation complete */
-    
-    //Escape Strings
-    $username = $mysqli->real_escape_string($username);
-    $password = password_hash($password, PASSWORD_DEFAULT);
-    $email = $mysqli->real_escape_string($email);
-    
-    //Register user in the database
-    $registration = $mysqli->query("INSERT INTO `$tableName`(username, email, passwordHash)"
-                                  . "VALUES('$username', '$email', '$password')") or die($mysqli->errorno());
-    $mysqli->close();
-    
-    //Registration complete
-    print("Registration successfull, thank you.\n");
-    return true;
-}
+$usernameRegex = getUsernameRegex();
 
 if (isset($_POST["submit"])) {
     registerUser($_POST["username"], $_POST["email"], $_POST["confirmEmail"], $_POST["password"], $_POST["confirmPassword"]) or die("invalid input");
 }
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
