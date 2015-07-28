c455dfacf945ed4166904ea967ea02981294ec79
Balonbal <olavbb@hotmail.com>
2015-07-28 04:39:44 +0200

    Added more input types for forms

--- resources/templates/form_template.php
+++ resources/templates/form_template.php
@@ -6,7 +6,7 @@
 	echo("<h4 style=\"color: red\">{$form["errorMessage"]}</h4>");
 } ?>
 <form action="<?php echo($form["action"]);?>" method="<?php echo($form["method"]); ?>" name="<?php echo($form["name"]); ?>">
-	<table style="margin: 0 auto 0 auto; text-align: right">
+	<table style="margin: 0 auto 0 auto; text-align: left" align="center">
 <?php foreach ($form["formObjects"] as $ID => $formObject): 
 	if ($formObject["type"] == "text" or $formObject["type"] == "password") { ?>
 		<tr>
@@ -14,6 +14,15 @@
 			<td><input type="<?php echo($formObject["type"]); ?>" name="<?php echo($ID); ?>"></td>
 		</tr>
 <?php }
+elseif ($formObject["type"] == "textarea") { ?>
+        <tr>
+            <td><label for="<?php echo ($ID); ?>"><?php echo($formObject["text"]); ?></label></td>
+            <td><textarea cols="<?php echo($formObject["cols"]); ?>" rows="<?php echo($formObject["rows"]); ?>" name="<?php echo($ID); ?>"></textarea></td>
+        </tr>
+        <?php
+} elseif ($formObject["type"] == "hidden") { ?>
+        <input type="hidden" name="<?php echo($ID); ?>" value="<?php echo($formObject["value"]); ?>" />
+        <?php }
 endforeach ?>
 	</table>
 	<input type="submit" name="submit"/>
