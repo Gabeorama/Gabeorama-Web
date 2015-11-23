a9d2b7108c4381923b0ccc571b3593c7d15ac053
Balonbal <olavbb@hotmail.com>
2015-11-23 17:29:53 +0100

    Updated header style

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -13,32 +13,31 @@
 </head>
 <body>
 <header id="header">
-    <h1><a href="<?php echo("$homeUrl"); ?>">GGR</a></h1>
-    <p>~(Gruppen tidligere kjent som Gabe's Game-O-Rama)~</p>
-</header>
-<div id="usernameBox">
+    <nav id="menu">
+        <ul>
+            <li><a href="<?php echo $homeUrl;?>">~</a></li>
+            <li><a href="/survey.php">Surveys</a></li>
+            <li><a href="/scripts"><p>scripts</p></a>
+                <ul>
+                    <li><a href="/scripts/linux"><p>GNU/Linux</p></a></li>
+                    <li><a href="/nuthin_here"><p>n/a</p></a></li>
+                </ul>
+            </li>
+            <li><a href="/events.php"><p>Events</p></a></li>
+        </ul>
+    </nav>
+    <div id="usernameBox">
         <?php
-    if (isset($_SESSION["username"])) {
+        if (isset($_SESSION["username"])) {
+            ?>
+            Logged in as <font color="orange"><?php print($_SESSION["username"])?></font>. <a href="logout.php">log out</a>
+            <?php
+        } else {
+            ?>
+            Not logged in. <a href="<?php echo("login.php?source=//$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"); ?>">Log in</a>
+            <?php
+        }
         ?>
-        Logged in as <font color="orange"><?php print($_SESSION["username"])?></font>. <a href="logout.php">log out</a>
-        <?php
-    } else {
-        ?>
-        Not logged in. <a href="<?php echo("login.php?source=//$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"); ?>">Log in</a>
-        <?php
-    }
-    ?>
     </div>
-<div id="menu">
-    <ul>
-        <li style="border-left: 2px solid red"><a href="<?php echo $homeUrl;?>"><p>Home</p></a></li>
-        <li><a href="/ihaxu"><p>U got Haxxed</p></a></li>
-        <li><a href="/scripts"><p>scripts</p></a>
-            <ul>
-                <li><a href="/scripts/linux"><p>GNU/Linux</p></a></li>
-                <li><a href="/nuthin_here"><p>n/a</p></a></li>
-            </ul>
-        </li>
-        <li><a href="/nuthin_here"><p>Another Element</p></a></li>
-    </ul>
-</div>
+</header>
+
