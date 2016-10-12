c86496d56c5af562e6639b0e2b82c4b8cbccc472
Olav Bech Bråten <olavbb@hotmail.com>
2016-10-12 21:32:24 +0200

    Added datetime picker

--- resources/templates/form_template.php
+++ resources/templates/form_template.php
@@ -1,12 +1,15 @@
 <?php 
 	/* Template for building forms */
 ?>
-<h1 xmlns="http://www.w3.org/1999/html"><?php echo $form["title"]; ?></h1>
+<h1><?php echo $form["title"]; ?></h1>
 <?php if (isset($form["errorMessage"])) { ?>
 	<div class="alert alert-danger">
         <strong>Error:</strong> <?php print($form["errorMessage"]); ?>
     </div>
 <?php } ?>
+
+<!-- actual form -->
+
 <form class="form-horizontal" action="<?php echo($form["action"]);?>" method="<?php echo($form["method"]); ?>" name="<?php echo($form["name"]); ?>">
     <?php foreach ($form["formObjects"] as $ID => $formObject):
         $type = $formObject["type"];
@@ -16,38 +19,85 @@
             $error = true;
             $errorMsg = $formObject["error"];
         }
-        if ($type == "text" or $type == "email" or $type == "password") { ?>
-    <div class="form-group <?php if ($error) print("has-error") ?>">
-        <label class="control-label col-sm-3" for="<?php print($ID); ?>"><?php print($formObject["text"]); ?></label>
-        <div class="col-sm-9">
-            <input name="<?php print($ID); ?>" type="<?php print($type);?>" class="form-control" id="<?php print($ID); ?>" <?php if ($error) print('aria-describedby="error' . $ID . '"');?>>
-        </div>
+
+        /** Simple text boxes */
+
+        if ($type == "text" or $type == "email" or $type == "password") {?>
+        <div class="form-group <?php if ($error) print("has-error") ?>">
+            <label class="control-label col-sm-3" for="<?php print($ID); ?>"><?php print($formObject["text"]); ?></label>
+            <div class="col-sm-9">
+                <input name="<?php print($ID); ?>" type="<?php print($type);?>" class="form-control" id="<?php print($ID); ?>" <?php if ($error) print('aria-describedby="error' . $ID . '"');?>>
+            </div>
         <?php if ($error) {?>
             <span id="error<?php print($ID); ?>" class="help-block col-sm-offset-3 col-sm-9"><?php print($errorMsg); ?></span>
         <?php } ?>
-    </div>
-        <?php } elseif ($type == "info" or $type == "error") {
+        </div>
+        <?php }
+
+        /** Info panels */
+
+        elseif ($type == "info" or $type == "error") {
             $modif = $type == "error" ? "danger" : $type;
             ?>
             <div class="alert alert-<?php print($modif); ?>">
                 <?php print($formObject["value"]); ?>
             </div>
-        <?php } elseif ($type == "header") {
+        <?php }
+
+        /** Section headers */
+
+        elseif ($type == "header") {
             ?>
             <h3><?php print($formObject["value"]); ?></h3>
         <?php } elseif ($type == "select") { ?>
-            <label class="control-label col-sm-3" for="<?php print($ID); ?>"><?php print($formObject["text"]); ?></label>
-            <div class="col-sm-9">
-                <select id="<?php print($ID); ?>" class="form-control" name="<?php print($ID);?>">
-                    <?php foreach ($formObject["value"] as $value): ?>
-                    <option><?php print($value); ?></option>
-                    <?php endforeach; ?>
-                </select>
+            <div class="form-group">
+                <label class="control-label col-sm-3" for="<?php print($ID); ?>"><?php print($formObject["text"]); ?></label>
+                <div class="col-sm-9">
+                    <select id="<?php print($ID); ?>" class="form-control" name="<?php print($ID);?>">
+                        <?php foreach ($formObject["value"] as $value): ?>
+                        <option><?php print($value); ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
             </div>
-        <?php } elseif ($type == "textarea") { ?>
-            <label class="control-label col-sm-3" for="<?php print($ID); ?>"><?php print($formObject["text"]); ?></label>
-            <div class="col-sm-9">
-                <textarea id="<?php print($ID); ?>" name="<?php print($ID); ?>" class="form-control col-sm-9" rows="<?php print $formObject["rows"]; ?>" style="margin-top: 0; margin-bottom: 10px"></textarea>
+        <?php }
+
+        /** Text Area */
+
+        elseif ($type == "textarea") { ?>
+            <div class="form-group">
+                <label class="control-label col-sm-3" for="<?php print($ID); ?>"><?php print($formObject["text"]); ?></label>
+                <div class="col-sm-9">
+                    <textarea id="<?php print($ID); ?>" name="<?php print($ID); ?>" class="form-control col-sm-9" rows="<?php print $formObject["rows"]; ?>" style="margin-top: 0; margin-bottom: 10px"></textarea>
+                </div>
+            </div>
+        <?php }
+
+        /** Date/time selection */
+
+        elseif ($type == "dateTime") { ?>
+            <div class="form-group">
+                <label class="control-label col-sm-3" for="<?php print($ID); ?>"><?php print($formObject["text"]); ?></label>
+                <div id="dateTime-<?php print($ID); ?>" class="input-group date col-sm-9">
+                    <input type="text" name="<?php print($ID); ?>" class="form-control" id="<?php print($ID); ?>">
+                    <span class="input-group-addon add-on">
+                        <i class="glyphicon glyphicon-calendar" data-time-icon="glyphicon glyphicon-time" data-date-icon="glyphicon glyphicon-calendar"></i>
+                    </span>
+                </div>
+                <script>
+                    $.getScript("http://<?php print($_SERVER['HTTP_HOST']); ?>/js/bootstrap-datetimepicker.min.js", function() {
+                        $.getScript("http://tarruda.github.com/bootstrap-datetimepicker/assets/js/bootstrap-datetimepicker.pt-BR.js", function() {
+                            $("#dateTime-<?php print($ID); ?>").datetimepicker({
+                                format: "yyyy-MM-dd hh:mm:ss",
+                                language: 'en',
+                                pickTime: true
+                            });
+
+                            var picker = $("#dateTime-<?php print($ID); ?>").data("datetimepicker");
+                            picker.setLocalDate(new Date());
+                        });
+                    });
+                </script>
             </div>
         <?php }
     endforeach; ?>
--- resources/templates/header.php
+++ resources/templates/header.php
@@ -11,6 +11,9 @@
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="//<?php print($_SERVER['HTTP_HOST']); ?>/css/stylesheet.css" type="text/css">
+    <link rel="stylesheet" type="text/css" media="screen" href="http://tarruda.github.com/bootstrap-datetimepicker/assets/css/bootstrap-datetimepicker.min.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <title><?php echo($title); ?></title>
 </head>
 <body>
